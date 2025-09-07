eaf71dedd4f130cbd6e9f1a6ae94425f4d282d88
Benjamin Macri <99494777+Duh-Inc@users.noreply.github.com>
2025-09-06 22:01:12 -0400

    Create items.py

---
+++ items.py
@@ -0,0 +1,42 @@
+import pygame
+import config
+
+class Tab_Items:
+
+    name = "ITEMS"
+
+    class Mode_Subtab:
+        def __init__(self, parent, name="Subtab"):
+            self.parent = parent
+            self.name = name
+            self.pageCanvas = pygame.Surface((config.WIDTH, config.HEIGHT))
+            self.changed = True
+
+        def drawPage(self):
+            if not self.changed:
+                return self.pageCanvas, False
+            self.pageCanvas.fill((30, 10, 10))
+            font = config.FONT_LRG
+            img = font.render(self.name, True, config.DRAWCOLOUR)
+            self.pageCanvas.blit(img, (50, 50))
+            self.changed = False
+            return self.pageCanvas, True
+
+        def resetPage(self):
+            self.changed = True
+
+        def ctrlEvents(self, events):
+            pass
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.modes = [self.Mode_Subtab(self, name) for name in ["Weapons", "Apparel", "Aid", "Misc", "Ammo"]]
+
+    def drawPage(self, modeNum):
+        return self.modes[modeNum].drawPage()
+
+    def resetPage(self, modeNum):
+        self.modes[modeNum].resetPage()
+
+    def ctrlEvents(self, events, modeNum):
+        self.modes[modeNum].ctrlEvents(events)
