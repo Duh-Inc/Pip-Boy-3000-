adee8d09cb24dda0b84bba41eb99d6822e2d748e
Benjamin Macri <99494777+Duh-Inc@users.noreply.github.com>
2025-09-07 22:06:22 -0400

    Update controls.py

--- controls.py
+++ controls.py
@@ -1,38 +1,29 @@
-# controls.py
 import pygame
 
 class Controls:
-    """
-    Stores the current input state.
-    Scroll wheel = navigation
-    Scroll button = select/play
-    Keys S/I/D = switch tabs
-    Keys 1-5 = switch subtabs
-    """
+    """Handle input emulation for Pip-Boy"""
     def __init__(self):
-        self.scrollDelta = 0
-        self.scrollButtonPressed = False
         self.tabKey = None
         self.subtabKey = None
+        self.scrollDelta = 0
+        self.scrollButtonPressed = False
 
     def update(self, events):
-        # reset values each frame
-        self.scrollDelta = 0
-        self.scrollButtonPressed = False
         self.tabKey = None
         self.subtabKey = None
+        self.scrollDelta = 0
+        self.scrollButtonPressed = False
 
         for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_s, pygame.K_i, pygame.K_d]:
+                    self.tabKey = event.key
+                elif event.key in [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5]:
+                    self.subtabKey = int(event.unicode)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:  # scroll up
                     self.scrollDelta = 1
                 elif event.button == 5:  # scroll down
                     self.scrollDelta = -1
-                elif event.button == 2:  # middle-click / scroll button
+                elif event.button == 2:  # middle-click
                     self.scrollButtonPressed = True
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key in (pygame.K_s, pygame.K_i, pygame.K_d):
-                    self.tabKey = event.key
-                elif event.key in (pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5):
-                    self.subtabKey = int(event.unicode)
